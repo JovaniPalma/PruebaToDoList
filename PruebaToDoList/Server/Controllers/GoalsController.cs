cfe7aee12f427ecde942bdf02eef57755c646cb2
Jovani Palma <jovanipo199@gmail.com.com>
2024-03-14 22:29:31 -0600

    Agrega(EnpointsGoals)

--- PruebaToDoList/Server/Controllers/GoalsController.cs
+++ PruebaToDoList/Server/Controllers/GoalsController.cs
@@ -1,9 +1,64 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PruebaToDoList.Server.DataBase;
+using PruebaToDoList.Shared.Enties;
 
 namespace PruebaToDoList.Server.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
     public class GoalsController : Controller
     {
-        
+        private readonly DataContext context;
+
+        public GoalsController(DataContext context)
+        {
+            this.context=context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Goal>>> GetGoals()
+        {
+            return await context.Goals.Include(x => x.Tasks).ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Goal>> PostGoals(Goal goal)
+        {
+            goal.Date = DateTime.Now;
+            context.Goals.Add(goal);
+            await context.SaveChangesAsync();
+            return Ok(goal);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<IEnumerable<Goal>>> PutGoals(Goal goal)
+        {
+            var goalDb = context.Goals.FirstOrDefault(x => x.Id == goal.Id);
+            if (goalDb is null)
+            {
+                return NotFound();
+            }
+
+            goalDb.Name = goal.Name;
+
+            await context.SaveChangesAsync();
+
+            return Ok(goal);
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult<Goal>> DeleteGoals(Goal goal)
+        {
+            context.Goals.Remove(goal);
+            await context.SaveChangesAsync();
+            return Ok(goal);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            context.Dispose();
+            base.Dispose(disposing);
+        }
     }
 }

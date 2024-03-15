7ca35a34d9d0719bb14aad4cbf510dd8b46dbb7f
Jovani Palma <jovanipo199@gmail.com.com>
2024-03-14 23:09:34 -0600

    Agrega(Conexion a api de tareas)

---
+++ PruebaToDoList/Server/Controllers/TasksController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PruebaToDoList.Server.DataBase;
+using PruebaToDoList.Shared.Enties;
+
+namespace PruebaToDoList.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TasksController : Controller
+    {
+        private readonly DataContext context;
+
+        public TasksController(DataContext context)
+        {
+            this.context=context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<TaskGoal>> PostGoals(TaskGoal task)
+        {
+            task.Date = DateTime.Now;
+            context.Tasks.Add(task);
+            await context.SaveChangesAsync();
+            return Ok(task);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<TaskGoal>> PutGoals(TaskGoal task)
+        {
+            var taskDb = context.Tasks.FirstOrDefault(x => x.Id == task.Id);
+            if (taskDb is null)
+            {
+                return NotFound();
+            }
+
+            taskDb.Name = task.Name;
+            taskDb.Important = task.Important;
+            taskDb.Estatus = task.Estatus;
+
+            await context.SaveChangesAsync();
+
+            return Ok(task);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult<Goal>> DeleteGoals(int id)
+        {
+            TaskGoal task = new()
+            {
+                Id = id
+            };
+            context.Tasks.Remove(task);
+            await context.SaveChangesAsync();
+            return Ok(task);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            context.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}

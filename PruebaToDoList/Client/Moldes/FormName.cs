ce15d4a348451bac0dfb251e4963416ed0f4b31e
Jovani Palma <jovanipo199@gmail.com.com>
2024-03-14 20:46:12 -0600

    Agrega(Editar y eliminar Meta)

---
+++ PruebaToDoList/Client/Moldes/FormName.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PruebaToDoList.Client.Moldes
+{
+    public class FormName
+    {
+        [Required(ErrorMessage = "Este campo es requerido")]
+        [MaxLength(80, ErrorMessage = "Maximo 80 caracteres")]
+        public string Name { get; set; } = string.Empty;
+    }
+}
